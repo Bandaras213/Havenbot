a1c68f27511c8a0997c591df26dea096f60b06b7
xTobiShotz <40842191+xTobiShotz@users.noreply.github.com>
2018-10-15 19:06:28 +0200

    Bully Fixes

--- commands/antiantibully.js
+++ commands/antiantibully.js
@@ -17,68 +17,66 @@ module.exports = async (bot, message, args, Discord, moment) => {
     const mes = await message.channel.send(`Akari wants to bully some nerds.`);
     let colorjson = "color.json";
     let lazytext = "Please use -Text -Color -Color"
-  
+
     let custom = args.join(" ").toUpperCase();
     const brgs = custom;
     const parts = brgs.split("-");
     parts.shift();
     let part1 = parts[0];
     let part2 = parts[1];
-    if (part1 === undefined){
-       part1 = lazytext
-    }else
-    if (part2 === undefined){
-    }else
-    {
+
+    if (part1 === undefined) {
+        part1 = lazytext
+    } else if (part2 === undefined) {
+    } else {
         part2 = parts[1].replace(/ /g, "");
-    } 
-    let part3 = parts[2];
-    if (part3 === undefined){
-    }else
-    {
-       part3 = parts[2].replace(/ /g, "");
-    }
-    let counter = 0;
-    for (let i = 0; i < parts.length; i ++)
-    if (parts[i] != null)
-        counter ++;
-  
-    let customText
-    switch(counter){
-	  case 0:
-	   return mes.edit(`${user} ${lazytext} for this command.`);
-	  case 1:
-	   customText = part1;
-        break;
-	  case 2:
-	   customText = part1;
-        break;
-    case 3:
-	   customText = part1;
-        break;
-    case 4:
-	   customText = part1;
-        break;
-    case 5: 
-     return mes.edit(`LOL it broke`);
     };
-   
+
+    let part3 = parts[2];
+    if (part3 === undefined) {
+    } else {
+        part3 = parts[2].replace(/ /g, "");
+    };
+
+    let counter = 0;
+    for (let i = 0; i < parts.length; i++)
+        if (parts[i] != null)
+            counter++;
+
+    let customText
+    switch (counter) {
+        case 0:
+            return mes.edit(`${user} ${lazytext} for this command.`);
+        case 1:
+            customText = part1;
+            break;
+        case 2:
+            customText = part1;
+            break;
+        case 3:
+            customText = part1;
+            break;
+        case 4:
+            customText = part1;
+            break;
+        case 5:
+            return mes.edit(`LOL it broke`);
+    };
+
     let color0
     if (part3 === undefined) {
-      color0 = "black";
-    }else
-    {
-      color0 = part3;
+        color0 = "black";
+    } else {
+        color0 = part3;
     };
 
     let color1
     if (part2 === undefined) {
-      color1 = "white";
-    }else
-    {
-      color1 = part2;
+        color1 = "white";
+    } else {
+        color1 = part2;
     };
-    
+
     console.log("The brgs String:", brgs);
     console.log("How many Strings are in brgs:", counter);
     console.warn("Start of the Parts variable");
@@ -89,25 +87,25 @@ module.exports = async (bot, message, args, Discord, moment) => {
     console.log("Start of the color strings");
     console.log("Color 1:", color0);
     console.log("Color 2:", color1);
-  
+
     const canvas = Canvas.createCanvas(400, 400);
     const ctx = canvas.getContext('2d');
-    const { body: buffer } = await Canvas.loadImage('./img/antiantibully.png');
+    const { body: buffer } = await Canvas.loadImage('./img/antiantibully.jpeg');
     const bg = await Canvas.loadImage(buffer);
     ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
-   
-    ctx.font = applyText(canvas, `${customText}`, 60,"bold");
+
+    ctx.font = applyText(canvas, `${customText}`, 60, "bold");
     ctx.strokeStyle = color0;
     ctx.fillStyle = color1;
     ctx.lineWidth = 6;
-    ctx.miterLimit=2;
+    ctx.miterLimit = 2;
     ctx.textAlign = "center";
     ctx.strokeText(`${customText}`, 200, 375);
     ctx.fillText(`${customText}`, 200, 375);
 
-    const attachment = new Discord.Attachment(canvas.toBuffer(), `antiantibullyranger.png`);    
+    const attachment = new Discord.Attachment(canvas.toBuffer(), `antiantibullyranger.png`);
     setTimeout(function () {
-    mes.delete();
-    message.channel.send(`${user} summoned the Anti Anti Bully Ranger`, (attachment));
+        mes.delete();
+        message.channel.send(`${user} summoned the Anti Anti Bully Ranger`, (attachment));
     }, 3500);
 }
